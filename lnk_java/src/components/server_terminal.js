887d878b3e605dc1b055cfe414d89bfe93c69b4d
Harman Dubb <harmand1999@gmail.com>
2023-04-26 23:58:03 -0700

    made the server terminal have an input field to send text responses to the server

--- lnk_java/src/components/server_terminal.js
+++ lnk_java/src/components/server_terminal.js
@@ -7,7 +7,18 @@ const ServerTerminal = (props) => {
       <div className="black-box">
         <p className="text">{props.initial}</p>
       </div>
-      <button className="send-button">Send</button>
+      <div className="input-container">
+        <input
+          type="text"
+          className="command-input"
+        //   value={command}
+        //   onChange={handleCommandChange}
+          placeholder="Enter command"
+        />
+        <button className="send-button"> {/* onClick={handleSubmit}> */}
+          Send
+        </button>
+      </div>
     </div>
   );
 };
