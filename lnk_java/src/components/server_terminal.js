19f9aee9073d958b1f485357638958cf4a7cedfe
Harman Dubb <harmand1999@gmail.com>
2023-04-27 21:20:20 -0700

    created the basic funtionaility for the server terminal

--- lnk_java/src/components/server_terminal.js
+++ lnk_java/src/components/server_terminal.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
+function ServerTerminal(props) {
+  const [command, setCommand] = useState('');
 
-const ServerTerminal = (props) => {
+  const handleCommandChange = (event) => {
+    setCommand(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Make a post to the back end for the server update
+    console.log(command);
+    setCommand('');
+  };
+
   return (
     <div className="container">
       <div className="black-box">
         <p className="text">{props.initial}</p>
+        {/* Communication block can be present here to make the terminal read out prompts and give answers as they are given */}
       </div>
       <div className="input-container">
         <input
           type="text"
+          name="userRequest"
           className="command-input"
-        //   value={command}
-        //   onChange={handleCommandChange}
+          value={command}
           placeholder="Enter command"
+          onChange={handleCommandChange}
         />
-        <button className="send-button"> {/* onClick={handleSubmit}> */}
+        <button className="send-button" onClick={handleSubmit}>
           Send
         </button>
       </div>
@@ -24,3 +38,7 @@ const ServerTerminal = (props) => {
 };
 
 export default ServerTerminal;
+
+function handleSubmit(){
+  setInputText()
+}
