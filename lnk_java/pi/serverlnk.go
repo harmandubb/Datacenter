ec2ad5f65e03816cacda4d193ee9bac012d62986
Harman Dubb <harmand1999@gmail.com>
2023-05-05 23:42:41 -0700

    Need to implement/check how the stand in function works

--- lnk_java/pi/serverlnk.go
+++ lnk_java/pi/serverlnk.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+
+	//"bytes"
+	"time"
 
 	"github.com/tarm/serial"
 )
@@ -30,42 +32,58 @@ func transmitCommand(command string) {
 
 	// Write data to the UART port.
 	data := []byte(command)
-	n, err := p.Write(data)
+	_, err = p.Write(data)
+	_, err = p.Write([]byte("\r"))
 	if err != nil {
 		log.Fatalf("Failed to write data to UART port: %v", err)
 	}
-	// fmt.Printf("Wrote %d bytes to UART port.\n", n)
+	//fmt.Printf("Wrote %d bytes to UART port.\n", n)
 
 	buf := make([]byte, 8192)
-	var response []byte
-	for {
-		n, err = p.Read(buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		response = append(response, buf[:n]...)
-
-		// Break the loop if a newline character is received.
-		if bytes.Contains(response, []byte("\n")) {
-			fmt.Println(string(response[:len(response)-1]))
-
-			break
-		}
+	//var response []byte
+	preVal := 0
+	currentVal := 1
+	time.Sleep(1 * time.Second)
+	// Check if the buffer size is changing
+	for preVal < currentVal {
+		preVal = currentVal
+		currentVal = len(buf)
+		time.Sleep(100 * time.Millisecond)
 	}
+
+	//for {
+	_, err = p.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(string(buf[:len(buf)]) + "\n")
+
+	//response = append(response, buf[:n]...)
+
+	// Break the loop if a newline character is received.
+	//if bytes.Contains(response, []byte("\n")) {
+	//time.Sleep(1*time.Second)
+	//fmt.Printf(string(response[:len(response)])+ "\n")
+	//for _, letter := range response {
+	//fmt.Println(string(letter))
+	//}
+
+	//break
+	//}
 
 }
 
 func main() {
 	fmt.Println("The GO Function is being executed")
 	if len(os.Args) < 2 {
-		fmt.Println("Not enough arguments present")
-		os.Exit(1)
+		//fmt.Println("Not enough arguments present")
+		//os.Exit(1)
+		transmitCommand("")
+		//transmitCommand("\r")
+	} else {
+		command := strings.Join(os.Args[1:], " ")
+
+		fmt.Println("echoing the command:", command)
+		transmitCommand(command)
 	}
-
-	command := strings.Join(os.Args[1:], " ")
-
-	// fmt.Println("echoing the command:", command)
-	transmitCommand(command)
-
 }
