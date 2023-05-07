afafb508422aa50a9d050a987a953d91648228b7
Harman Dubb <harmand1999@gmail.com>
2023-05-07 14:01:23 -0700

    working versin of the serverlnk communication program

--- lnk_java/pi/serverlnk.go
+++ lnk_java/pi/serverlnk.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
-
-	//"bytes"
 	"time"
 
 	"github.com/tarm/serial"
@@ -37,7 +38,7 @@ func transmitCommand(command string) {
 	if err != nil {
 		log.Fatalf("Failed to write data to UART port: %v", err)
 	}
-	//fmt.Printf("Wrote %d bytes to UART port.\n", n)
+	fmt.Printf("Wrote %d bytes to UART port.\n", n)
 
 	buf := make([]byte, 8192)
 	//var response []byte
@@ -50,31 +51,45 @@ func transmitCommand(command string) {
 		currentVal = len(buf)
 		time.Sleep(100 * time.Millisecond)
 	}
-
 	//for {
 	_, err = p.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf(string(buf[:len(buf)]) + "\n")
+	fmt.Printf("The buffer holds:", string(buf[:len(buf)])+"\n")
+	fileName := "/home/read.txt"
+	buf = bytes.Trim(buf[:len(buf)], "\x00")
+	buf = bytes.TrimLeft(buf[:len(buf)], "\x0a")
+	buf = bytes.TrimLeft(buf[:len(buf)], "\x0d")
+	buf = bytes.TrimLeft(buf[:len(buf)], "\x0a")
+	buf = append(buf, byte('\n'))
+	fmt.Println(buf[:len(buf)])
+	content := string(buf[:len(buf)])
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//response = append(response, buf[:n]...)
+	err = ioutil.WriteFile(fileName, []byte(content), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Break the loop if a newline character is received.
-	//if bytes.Contains(response, []byte("\n")) {
-	//time.Sleep(1*time.Second)
-	//fmt.Printf(string(response[:len(response)])+ "\n")
-	//for _, letter := range response {
-	//fmt.Println(string(letter))
-	//}
+	fmt.Printf("Successfully wrote to file: %s \nThe content: %s\n", fileName, content)
 
-	//break
-	//}
-
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	fmt.Println("The GO Function is being executed")
+	//app := "echo"
+	//arg := "the GO Function is being executied"
+	//cmd := exec.Command(app, arg)
+	//output, _ := cmd.Output()
+	//fmt.Println(string(output))
+	//os.Stdout.Write([]byte("The GO Function is being executed"))
 	if len(os.Args) < 2 {
 		//fmt.Println("Not enough arguments present")
 		//os.Exit(1)
@@ -83,7 +98,16 @@ func main() {
 	} else {
 		command := strings.Join(os.Args[1:], " ")
 
-		fmt.Println("echoing the command:", command)
+		fmt.Println("echoing the input command:", command)
 		transmitCommand(command)
 	}
+	//attempt to read commands from the file
+	app := "bash"
+	arg0 := "-c"
+	arg1 := "while read line; do echo $line; done < read.txt"
+	cmd := exec.Command(app, arg0, arg1)
+	output, _ := cmd.Output()
+	//fmt.Println("Size of the read file:", len(output))
+	fmt.Println(string(output))
+
 }
