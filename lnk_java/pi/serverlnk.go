f47da86bbc00994211ba3aba7e1a304399a2c3e9
Harman Dubb <harmand1999@gmail.com>
2023-05-07 15:12:26 -0700

    Got the system working such that the box returns the correct server input

--- lnk_java/pi/serverlnk.go
+++ lnk_java/pi/serverlnk.go
@@ -38,7 +38,7 @@ func transmitCommand(command string) {
 	if err != nil {
 		log.Fatalf("Failed to write data to UART port: %v", err)
 	}
-	fmt.Printf("Wrote %d bytes to UART port.\n", n)
+	//fmt.Printf("Wrote %d bytes to UART port.\n", n)
 
 	buf := make([]byte, 8192)
 	//var response []byte
@@ -51,19 +51,19 @@ func transmitCommand(command string) {
 		currentVal = len(buf)
 		time.Sleep(100 * time.Millisecond)
 	}
-	//for {
 	_, err = p.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("The buffer holds:", string(buf[:len(buf)])+"\n")
+	//fmt.Printf("The buffer holds: %s \n",string(buf[:len(buf)]))
 	fileName := "/home/read.txt"
 	buf = bytes.Trim(buf[:len(buf)], "\x00")
 	buf = bytes.TrimLeft(buf[:len(buf)], "\x0a")
 	buf = bytes.TrimLeft(buf[:len(buf)], "\x0d")
 	buf = bytes.TrimLeft(buf[:len(buf)], "\x0a")
+	buf = buf[len(command)+2:]
 	buf = append(buf, byte('\n'))
-	fmt.Println(buf[:len(buf)])
+	//fmt.Println(buf[:len(buf)])
 	content := string(buf[:len(buf)])
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -75,7 +75,7 @@ func transmitCommand(command string) {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Successfully wrote to file: %s \nThe content: %s\n", fileName, content)
+	//fmt.Printf("Successfully wrote to file: %s \nThe content: %s\n",fileName,content)
 
 	err = file.Close()
 	if err != nil {
@@ -84,30 +84,22 @@ func transmitCommand(command string) {
 }
 
 func main() {
-	//app := "echo"
-	//arg := "the GO Function is being executied"
-	//cmd := exec.Command(app, arg)
-	//output, _ := cmd.Output()
-	//fmt.Println(string(output))
-	//os.Stdout.Write([]byte("The GO Function is being executed"))
+	//fmt.Println("Entered the serverlnk.go file")
 	if len(os.Args) < 2 {
-		//fmt.Println("Not enough arguments present")
-		//os.Exit(1)
 		transmitCommand("")
-		//transmitCommand("\r")
+
 	} else {
 		command := strings.Join(os.Args[1:], " ")
 
-		fmt.Println("echoing the input command:", command)
+		//fmt.Println("echoing the input command:", command)
 		transmitCommand(command)
 	}
 	//attempt to read commands from the file
 	app := "bash"
 	arg0 := "-c"
-	arg1 := "while read line; do echo $line; done < read.txt"
+	arg1 := "while read line; do echo $line; done < /home/read.txt"
 	cmd := exec.Command(app, arg0, arg1)
 	output, _ := cmd.Output()
-	//fmt.Println("Size of the read file:", len(output))
-	fmt.Println(string(output))
+	fmt.Printf("%s", string(output))
 
 }
