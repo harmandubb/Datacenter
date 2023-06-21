d025c15bda57d90cfd308bb130c950f33c2da2ef
harmandubb <harmand1999@gmail.com>
2023-06-20 23:43:31 -0700

    Got the transmission packet to correctly transmit eventually.

--- spi.c
+++ spi.c
@@ -42,10 +42,18 @@ uint8_t bits = 8;
 #define MAMXFLHREG 0x0B
 #define MABBIPGREG 0x04
 #define MAIPGLREG 0x06 
+#define EPKTCNTREG 0x19
+#define IPV4TYPE 0x0800
+#define ADDRESSRESOLUTIONPROTOCOL 0x0806
+#define IPV6TYPE 0x86DD
 
 void checkReg(int fd, uint8_t reg, char name[]);
 void splitByte(uint8_t byte, uint8_t* split);
-
+void splitWord(uint16_t word, uint8_t* split);
+uint8_t* IPV4PacketCreator(uint8_t payload[], uint8_t srcIPAddress[], uint8_t destIPAddress[], int* packetLen);
+uint16_t IPV4CheckSumCalculate(uint8_t version, uint8_t headerLength, uint8_t typeOfService, uint16_t totalLength, uint16_t identification, uint8_t flags, uint16_t fragmentOffset, uint8_t timeToLive, uint8_t Protocol, uint8_t srcIPAddress[], uint8_t destIPAddress[]);
+uint8_t* TCPHeaderCreator(uint16_t srcPort, uint16_t destPort, uint32_t sequenceNum, uint32_t ackNum, uint8_t* data, int dataLen, int* TCPHeaderLen);
+uint16_t TCPCheckSumCalculator(uint8_t* TCPHeader, int* TCPHeaderLen);
 
 /**
  * @brief Funciton makes a SPI transfer 
@@ -180,13 +188,15 @@ void resetCommand(int fd){
 void enableInterrupts(int fd){
         uint8_t data[2] = {0};
 
-        checkReg(fd, EIRREG, "EIR");
 
         //Enable Global interupt bit 
         bitSet(fd,data,EIEREG,0x80);
 
         //Enable interupts for packet transmit and transmit error
         bitSet(fd, data, EIEREG, 0x02+0x08);
+
+        //Enable interupts for packet receive and receive error 
+        bitSet(fd, data, EIEREG, 0x01+0x40);
 
         //Check interupt enable register
         checkReg(fd,EIEREG, "EIE");
@@ -293,8 +303,8 @@ void spiInitilization(int fd, uint8_t macAddressLocal[6]){
         readControlReg(fd, data, MACON3REG);
         uint8_t MACON3_Val = data[1];
 
-        // MACON3_Val = 0xF2;
-        MACON3_Val = 0x12;
+        MACON3_Val = 0xF2; //includes padding
+        // MACON3_Val = 0x12; //excludes padding
 
         writeControlReg(fd,data,MACON3REG,MACON3_Val);
 
@@ -358,7 +368,7 @@ void clearInterupt(int fd, uint8_t interuptBit){
         bitSet(fd,data,EIEREG,0x80);
 }
 
-uint16_t transmitPacket(int fd, uint16_t* trasmitStartPtr, uint8_t srcMACAddress[], char message[]){
+uint16_t transmitPacket(int fd, uint16_t* trasmitStartPtr, uint8_t srcMACAddress[], uint8_t* message, int messageLen){
         uint8_t data[2] = {0};
         uint8_t split[2];
 
@@ -366,9 +376,8 @@ uint16_t transmitPacket(int fd, uint16_t* trasmitStartPtr, uint8_t srcMACAddress
         int destMACAddressLen = 6;
         int srcMACAddressLen = 6;
         int typeLen = 2;
-        int messageLen = strlen(message);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //set the trasmit start pointer 
         splitWord(*trasmitStartPtr, split);
@@ -376,42 +385,36 @@ uint16_t transmitPacket(int fd, uint16_t* trasmitStartPtr, uint8_t srcMACAddress
         writeControlReg(fd, data, ETXSTLREG, split[1]);
         writeControlReg(fd, data, ETXSTHREG, split[0]);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         // Write a control byte
         data[0] = 0x0E;
         writeBufferMemory(fd, data, 1);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         // destination address 
         uint8_t destMACAddress[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
         writeBufferMemory(fd, destMACAddress, 6);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //source address        
         writeBufferMemory(fd, srcMACAddress, 6);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //type/length
-        // splitWord(messageLen, split);
-        // splitWord(68, split);
-        // splitWord(64, split);
-        // splitWord(60, split);
-        // splitWord(48, split);
-        // splitWord(12, split);
-        splitWord(11, split);
+        splitWord(IPV4TYPE, split);
 
         writeBufferMemory(fd, split, 2);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //message
         writeBufferMemory(fd, message, messageLen);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //set the trasmit end pointer
         uint16_t transmitEndPtr = *trasmitStartPtr + controlByteLen + destMACAddressLen + srcMACAddressLen + typeLen + messageLen - 1;
@@ -420,17 +423,17 @@ uint16_t transmitPacket(int fd, uint16_t* trasmitStartPtr, uint8_t srcMACAddress
         writeControlReg(fd, data, ETXNDLREG, split[1]);
         writeControlReg(fd, data, ETXNDHREG, split[0]);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         // Clear EIR.TXIF
         bitClear(fd, data, EIRREG, 0x08);
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //set EIE.TXIE and EIE.INTIE
         bitSet(fd, data, EIEREG, 0x88);
-        checkReg(fd, EIEREG, "EIE");
+        // checkReg(fd, EIEREG, "EIE");
 
-        checkReg(fd, ECON1REG, "ECON1");
+        // checkReg(fd, ECON1REG, "ECON1");
         
         //Clear the transmit reset and receive reset bits
         bitClear(fd, data, ECON1REG, 0x80 + 0x40);
@@ -481,11 +484,10 @@ void readTransmissionMessageBuffer(int fd, uint8_t transmitStartPtr, char messag
         free(data);
 }
 
-void readTransmissionPacket(int fd, uint8_t transmitStartPtr, char message[]){
+void readTransmissionPacket(int fd, uint8_t transmitStartPtr, int messageLen){
         uint8_t split[2];
         int transmissionPacketBaseSize = 15;
         int statusVectorSize = 7;
-        int messageLen = strlen(message);
         int packetSize = messageLen+transmissionPacketBaseSize+statusVectorSize;
         uint8_t* data = (uint8_t*) calloc(packetSize, sizeof(uint8_t));
 
@@ -537,7 +539,7 @@ bool verifyTransmission(int fd, uint8_t transmitEndPtr){
         //clear the EIR.TXIF interrupt flag
         clearInterupt(fd, 0x08);
 
-        checkReg(fd, EIRREG, "EIR");
+        // checkReg(fd, EIRREG, "EIR");
 
         //check if ESTAT.TXABRT is clear
         readControlReg(fd,data,ESTATREG);
@@ -564,6 +566,241 @@ bool verifyTransmission(int fd, uint8_t transmitEndPtr){
         return success;
 }
 
+void enableReceive(int fd){
+        uint8_t data[2] = {0};
+        bitSet(fd,data,ECON1REG,0x04);
+
+        //at this point the receive ptrs should not be modified until the receive is complete
+}
+
+bool receivePacketAvailable(int fd){
+        bool available = false; 
+        uint8_t data[2] = {0};
+
+        switchRegBank(fd,1);
+        readControlReg(fd,data,EPKTCNTREG);
+
+        uint8_t EPKTCNT_Val = data[1];
+
+        if (EPKTCNT_Val > 0){
+                available = true;
+        }
+
+        return available;
+}
+
+uint16_t mergeWord(uint8_t highByte, uint8_t lowByte){
+        uint16_t merged = 0;
+        merged = (highByte << 8) | lowByte;
+        return merged;
+}
+
+uint8_t* serviceReceivePacket(int fd, uint16_t* packetPtr){
+        uint8_t data[14] = {0};
+        uint8_t split[2];
+        uint8_t crcData[4] = {0};
+
+        //set the read pointer to the start of the receive packet 
+        splitWord(*packetPtr, split);
+        switchRegBank(fd,0);
+        writeControlReg(fd,data,ERDPTLREG,split[1]);
+        writeControlReg(fd,data,ERDPTHREG,split[0]);
+
+        //read the first two byte to get the next packet pointer 
+        readBufferMemory(fd,data,2);
+        *packetPtr = mergeWord(data[1],data[0]);
+
+        //read the receive status Vector 
+        readBufferMemory(fd,data,7);
+
+        //print the contents of the status vector
+        printf("Receive Status Vector: \n");
+        for (int i = 0; i < 7; i++){
+                printf("%02X ", data[i]);
+        }
+        printf("\n");
+
+        //read the receive headers at the next 14 bytes
+        readBufferMemory(fd,data,14);
+
+        //extract the length of the message 
+        uint16_t messageLen = mergeWord(data[12],data[13]);
+
+        //malloc space for the message data
+        uint8_t* message = (uint8_t*) calloc(messageLen, sizeof(uint8_t));
+        //read the message
+        readBufferMemory(fd,message,messageLen);
+
+        //read in the crc data 
+        readBufferMemory(fd,crcData,4);
+
+        return message;
+}
+
+uint8_t* IPV4PacketCreator(uint8_t payload[], uint8_t srcIPAddress[], uint8_t destIPAddress[], int* packetLen){
+        uint16_t srcPort = 80;
+        uint16_t destPort = 22;
+        uint32_t seqNum = 0;
+        uint32_t ackNum = 0;
+        int TCPPacketLen = 0;
+        uint8_t TCPPacket = TCPHeaderCreator(srcPort, destPort, seqNum,ackNum, payload, strlen(payload), &TCPPacketLen);
+
+        *packetLen = 20 + TCPPacketLen;
+
+        uint8_t* packet = (uint8_t*) calloc(*packetLen, sizeof(uint8_t));
+        int dataSegmentDivide = 1400;
+
+        uint8_t version = 4;
+        uint8_t headerLength = 5; 
+        uint8_t typeOfService = 0;
+        uint16_t totalLength = *packetLen;
+
+        if (totalLength > dataSegmentDivide){
+                printf("Payload is too large\n");
+        }       
+
+        uint16_t identification = 0;
+        uint8_t flags = 0;
+        uint16_t fragmentOffset = 0; //num bytes divided by 8 
+
+        uint8_t timeToLive = 64;
+        uint8_t protocol = 6; //TCP
+
+        uint16_t checksum = IPV4CheckSumCalculate(version, headerLength, typeOfService, totalLength, identification, flags, fragmentOffset, timeToLive, protocol, srcIPAddress, destIPAddress);
+
+        packet[0] = (version << 4) | headerLength;
+        packet[1] = typeOfService;
+        packet[2] = totalLength >> 8;
+        packet[3] = totalLength & 0xFF;
+        packet[4] = identification >> 8;
+        packet[5] = identification & 0xFF;
+        packet[6] = (flags << 5) | fragmentOffset >> 8;
+        packet[7] = fragmentOffset & 0xFF;
+        packet[8] = timeToLive;
+        packet[9] = protocol;
+        packet[10] = checksum >> 8;
+        packet[11] = checksum & 0xFF;
+        packet[12] = srcIPAddress[0];
+        packet[13] = srcIPAddress[1];
+        packet[14] = srcIPAddress[2];
+        packet[15] = srcIPAddress[3];
+        packet[16] = destIPAddress[0];
+        packet[17] = destIPAddress[1];
+        packet[18] = destIPAddress[2];
+        packet[19] = destIPAddress[3];
+
+        
+
+        
+
+        for(int i = 0; i < TCPPacketLen; i++){
+                packet[20+i] = payload[i];
+        }
+
+        printf("IPV4 Packet: \n");
+        for (int i = 0; i< *packetLen; i++){
+                printf("%02X ", packet[i]);
+        }
+        printf("\n");
+
+        return packet;
+}
+
+uint16_t IPV4CheckSumCalculate(uint8_t version, uint8_t headerLength, uint8_t typeOfService, uint16_t totalLength, uint16_t identification, uint8_t flags, uint16_t fragmentOffset, uint8_t timeToLive, uint8_t Protocol, uint8_t srcIPAddress[], uint8_t destIPAddress[]){
+        uint16_t words[9] = {0};
+        words[0] = (version << 12) | (headerLength << 8) | typeOfService;
+        words[1] = totalLength;
+        words[2] = identification;
+        words[3] = (flags << 13) | fragmentOffset;
+        words[4] = (timeToLive << 8) | Protocol;
+        words[5] = srcIPAddress[0] << 8 | srcIPAddress[1];
+        words[6] = srcIPAddress[2] << 8 | srcIPAddress[3];
+        words[7] = destIPAddress[0] << 8 | destIPAddress[1];
+        words[8] = destIPAddress[2] << 8 | destIPAddress[3];
+
+        uint32_t sum = 0;
+        for (int i = 0; i < 9; i++){
+                sum += words[i];
+        }
+
+        //folding the 32 bit number into a 16 bit number 
+        uint16_t checksum = (sum >> 16) + (sum & 0xFFFF);
+        checksum = ~checksum;
+
+        printf("Checksum: %04X\n", checksum);
+
+        return checksum;
+}
+
+uint8_t* TCPHeaderCreator(uint16_t srcPort, uint16_t destPort, uint32_t sequenceNum, uint32_t ackNum, uint8_t* data, int dataLen, int* TCPHeaderLen){
+        *TCPHeaderLen = 20+dataLen+dataLen%2;
+        uint8_t* TCPHeader = (uint8_t*) calloc(*TCPHeaderLen, sizeof(uint8_t));
+        
+        bool CWR = false; //Congestion window reduced
+        bool ECE = false; //Echo 
+        bool URG = false; //Urgent Point active 
+        bool ACK = false; //Acknowledgement field is to be read
+        bool PSH = false; //Push function
+        bool RST = false; //Reset the connection
+        bool SYN = true; //Synchronize sequence numbers: active on the first packet
+        bool FIN = true; //Last packet from sender 
+
+        TCPHeader[0] = srcPort >> 8;
+        TCPHeader[1] = srcPort & 0xFF;
+        TCPHeader[2] = destPort >> 8;
+        TCPHeader[3] = destPort & 0xFF;
+        TCPHeader[4] = sequenceNum >> 24;
+        TCPHeader[5] = (sequenceNum >> 16) & 0xFF;
+        TCPHeader[6] = (sequenceNum >> 8) & 0xFF;
+        TCPHeader[7] = sequenceNum & 0xFF;
+        TCPHeader[8] = ackNum >> 24;
+        TCPHeader[9] = (ackNum >> 16) & 0xFF;
+        TCPHeader[10] = (ackNum >> 8) & 0xFF;
+        TCPHeader[11] = ackNum & 0xFF;
+        TCPHeader[12] = 5 << 4;
+        TCPHeader[13] = (uint8_t) CWR << 7 | (uint8_t) ECE << 6 | (uint8_t) URG << 5 | (uint8_t) ACK << 4 | (uint8_t) PSH << 3 | (uint8_t) RST << 2 | (uint8_t) SYN << 1 | (uint8_t) FIN;
+        TCPHeader[14] = 0xFF;
+        TCPHeader[15] = 0xFF;
+        TCPHeader[18] = 0;
+        TCPHeader[19] = 0;
+
+        for(int i = 0; i < dataLen; i++){
+                TCPHeader[20+i] = data[i];
+        }
+
+        if (dataLen%2){
+                TCPHeader[20+dataLen] = 0;
+        }
+
+        //calculate the checksum to be inserted in the tcp header
+        uint16_t checksum = TCPCheckSumCalculator(TCPHeader, TCPHeaderLen);
+        TCPHeader[16] = (uint8_t) (checksum >> 8);
+        TCPHeader[17] = (uint8_t) (checksum & 0xFF);
+
+        // print out the TCP header
+        printf("TCP Header: \n");
+        for (int i = 0; i< *TCPHeaderLen; i++){
+                printf("%02X ", TCPHeader[i]);
+        }
+        printf("\n");
+
+        return TCPHeader;
+}
+
+uint16_t TCPCheckSumCalculator(uint8_t* TCPHeader, int* TCPHeaderLen){
+        uint32_t sum = 0;
+        for(int i = 0; i < *TCPHeaderLen/2; i++){
+                sum += TCPHeader[2*i] << 8 | TCPHeader[2*i+1];
+        }
+
+        //folding the 32 bit number into a 16 bit number 
+        uint16_t checksum = (sum >> 16) + (sum & 0xFFFF);
+        checksum = ~checksum;
+
+        printf("Checksum: %04X\n", checksum);
+
+        return checksum;
+}
 
 
 int main(int argc, char * argv[]){
@@ -601,27 +838,53 @@ int main(int argc, char * argv[]){
         resetCommand(fd);
 
         //88:63:df:b0:af:ce
-        uint8_t srcMacAddress[6] = {0x88,0x63,0xDF,0xB0,0xAF,0xCE};
+        uint8_t srcMacAddress[] = {0x88,0x63,0xDF,0xB0,0xAF,0xCE};
+        uint8_t srcIPAddress[] = {169,254,250,95};
+        uint8_t destIPAddress[] = {192,168,1,255};
 
         spiInitilization(fd,srcMacAddress);
 
         enableInterrupts(fd);
 
-        checkReg(fd, EIRREG, "EIR");
+        enableReceive(fd);
 
-        
+        int counter = 0; 
 
-        char transmissionMessage[] = "Hello World";
+        while(counter < 1){
+
+        uint16_t receiveBufferPtr = RECEIVEBUFFERSTART;
+
+        char transmissionMessage[] = "Check if this tranmission of TCP is working?";
 
         uint16_t transmitStartPtr = 0x0000;
 
-        uint16_t transmitEndPtr = transmitPacket(fd, &transmitStartPtr, srcMacAddress, transmissionMessage);
+        int packetLen;
+        uint8_t* packet = IPV4PacketCreator(transmissionMessage,srcIPAddress,destIPAddress, &packetLen);
+        
+        uint16_t transmitEndPtr = transmitPacket(fd, &transmitStartPtr, srcMacAddress, packet, packetLen);
 
-        readTransmissionPacket(fd,transmitStartPtr,transmissionMessage);
-
-        // readTransmissionMessageBuffer(fd,transmitStartPtr,transmissionMessage);
+        readTransmissionPacket(fd,transmitStartPtr,packetLen);
 
         success = verifyTransmission(fd,transmitEndPtr);
+
+        if (!success){
+                printf("Transmission Failed\n");
+                return -1;
+        } else {
+                printf("Transmission Successful\n");
+
+                // while(1){
+                //         if (receivePacketAvailable(fd)){
+                //                 uint8_t* message = serviceReceivePacket(fd,&receiveBufferPtr);
+                //                 printf("Message: %s\n", message);
+                //                 free(message);
+                //         }
+                // }
+        }
+
+        counter++;
+
+        }
 
 
 
